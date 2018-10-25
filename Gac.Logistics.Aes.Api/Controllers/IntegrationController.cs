d9d8280c54a591b51454a1ff4ab9d8aa2582c2b5
Subindev Vasudevan <subindev.vasudevan@gac.com>
2018-10-25 13:50:24 +0400

    integration api

---
+++ Gac.Logistics.Aes.Api/Controllers/IntegrationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Gac.Logistics.Aes.Api.Data;
+using Gac.Logistics.Aes.Api.Model.Acknowledgements;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gac.Logistics.Aes.Api.Controllers
+{
+    [Route("api/integration")]
+    [ApiController]
+    public class IntegrationController : ControllerBase
+    {
+        private readonly AesDbRepository aesDbRepository;
+        private readonly IMapper mapper;
+
+        public IntegrationController(AesDbRepository aesDbRepository, IMapper mapper)
+        {
+            this.aesDbRepository = aesDbRepository;
+            this.mapper = mapper;
+        }
+
+        // POST used by GF
+        [HttpPost("ackgetsresponse")]
+        public ActionResult AckGetsResponse(AckGetsReponse getsResponse)
+        {
+            return Ok(true);
+        }
+
+        [HttpPost("ackaescustomsresponse")]
+        public ActionResult AckAesCustomsResponse(dynamic customsReponse)
+        {
+            return Ok(true);
+        }
+    }
+}
---
+++ Gac.Logistics.Aes.Api/Model/Acknowledgements/AckGetsReponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gac.Logistics.Aes.Api.Model.Acknowledgements
+{
+    public class AckCustomsReponse
+    {
+        public Ack ACK { get; set; }
+        public ErrorMessage Error { get; set; }
+    }
+
+    public class AckGetsReponse
+    {
+        public Ack ACK { get; set; }
+        public ErrorMessage Error { get; set; }
+    }
+
+    public class Ack
+    {
+        public string ShipmentReferenceNumber { get; set; }
+        public string Status { get; set; }
+    }
+
+    public class ErrorMessage
+    {
+        public string ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+    }
+}
