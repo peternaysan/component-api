138604b2491972f9b17f3bf578193b9d4ab6ec00
Jyothsna Shriyan <jyothsna.shriyan@gac.com>
2018-11-06 16:23:50 +0400

    added customs response class

---
+++ Gac.Logistics.Aes.Api/Model/Acknowledgements/AesCustomsResponse.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gac.Logistics.Aes.Api.Model.Acknowledgements
+{
+    public class AesCustomsResponse
+    {
+        public Ftpaesresponse ftpaesResponse { get; set; }
+    }
+
+    public class Ftpaesresponse
+    {
+        public Ftpcommodityshipment ftpcommodityShipment { get; set; }
+    }
+
+    public class Ftpcommodityshipment
+    {
+        public Ftpshipmentheader ftpshipmentHeader { get; set; }
+        public Ftpshipmentheaderresponse ftpshipmentHeaderResponse { get; set; }
+        public Ftpcommoditylineitemgroup ftpcommodityLineItemGroup { get; set; }
+    }
+
+    public class Ftpshipmentheader
+    {
+        public string shipmentReferenceNumber { get; set; }
+    }
+
+    public class Ftpshipmentheaderresponse
+    {
+        public string responseCode { get; set; }
+        public string finalDispositionIndicator { get; set; }
+        public string narrativeText { get; set; }
+        public string internalTransactionNumber { get; set; }
+    }
+
+    public class Ftpcommoditylineitemgroup
+    {
+        public Ftplineitemheadercontinuationresponse[] ftplineItemHeaderContinuationResponse { get; set; }
+    }
+
+    public class Ftplineitemheadercontinuationresponse
+    {
+        public string responseCode { get; set; }
+        public string severityIndicator { get; set; }
+        public string narrativeText { get; set; }
+        public string finalDispositionIndicator { get; set; }
+        public string internalTransactionNumber { get; set; }
+    }
+
+}
