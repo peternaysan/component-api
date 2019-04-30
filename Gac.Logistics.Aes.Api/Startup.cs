8d46195adab474f953f668a79d63bacf39c634a4
Jyothsna Shriyan <jyothsna.shriyan@gac.com>
2019-04-30 08:02:16 +0400

    added empty string to null converter

--- Gac.Logistics.Aes.Api/Startup.cs
+++ Gac.Logistics.Aes.Api/Startup.cs
@@ -43,11 +43,10 @@ namespace AesComponentApi
             services.AddTransient<IxService, IxService>();
 
             services.AddMvc()
-                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-                    //.AddJsonOptions(options =>
-                    //                    options.SerializerSettings.Converters
-                    //                           .Add(new EmptyStringToNullJsonConverter()));
-
+                    .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                    .AddJsonOptions(options =>
+                                        options.SerializerSettings.Converters
+                                               .Add(new EmptyStringToNullJsonConverter()));
             services.AddCors(options =>
                              {
                                  options.AddPolicy("CorsPolicy",
@@ -108,7 +107,7 @@ namespace AesComponentApi
                 routes.MapHub<AesHub>("/aesHub");
             });
             app.UseMvc();
-            
+
 
         }
     }
